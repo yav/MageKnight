375a7958109f2a287889ba6af9aa0809118334ca
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-01-14 22:42:46 -0800

    Add remaining enemies

--- MageKnight/Enemies.hs
+++ MageKnight/Enemies.hs
@@ -36,9 +36,15 @@ data Enemy = Enemy
   , enemyAbilities  :: Set EnemyAbility
   }
 
+instance Eq Enemy where
+  x == y = enemyName x == enemyName y
+
+instance Ord Enemy where
+  compare x y = compare (enemyName x) (enemyName y)
+
 
 allEnemies :: [Enemy]
-allEnemies = orcs ++ keep ++ dungeon
+allEnemies = orcs ++ keep ++ dungeon ++ magical ++ draconum ++ citizens
 
 
 orcs :: [Enemy]
@@ -191,3 +197,146 @@ dungeon = concat
   ]
 
 
+
+magical :: [Enemy]
+magical = concat
+  [ replicate 2 Enemy
+      { enemyName      = "Monks"
+      , enemyType      = Mage
+      , enemyArmour    = 5
+      , enemyAbilities = Set.fromList [ Poisons ]
+      , enemyAttack    = AttcaksWith Physycal 5
+      , enemyFameGain  = 4
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Illusionists"
+      , enemyType      = Mage
+      , enemyArmour    = 3
+      , enemyAbilities = Set.fromList [ Resists Physycal ]
+      , enemyAttack    = Summoner
+      , enemyFameGain  = 4
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Ice Mages"
+      , enemyType      = Mage
+      , enemyArmour    = 6
+      , enemyAbilities = Set.fromList [ Resists Ice ]
+      , enemyAttack    = AttcaksWith Ice 5
+      , enemyFameGain  = 5
+      }
+
+  , replicate 1 Enemy
+      { enemyName      = "Ice Golems"
+      , enemyType      = Mage
+      , enemyArmour    = 4
+      , enemyAbilities = Set.fromList [ Resists Physycal, Resists Ice,
+                                                                Paralyzes ]
+      , enemyAttack    = AttcaksWith Ice 2
+      , enemyFameGain  = 5
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Fire Mages"
+      , enemyType      = Mage
+      , enemyArmour    = 5
+      , enemyAbilities = Set.fromList [ Resists Fire ]
+      , enemyAttack    = AttcaksWith Fire 6
+      , enemyFameGain  = 5
+      }
+
+  , replicate 1 Enemy
+      { enemyName      = "Fire Golems"
+      , enemyType      = Mage
+      , enemyArmour    = 4
+      , enemyAbilities = Set.fromList [ Resists Physycal, Resists Fire, Brutal ]
+      , enemyAttack    = AttcaksWith Fire 3
+      , enemyFameGain  = 5
+      }
+
+  ]
+
+
+draconum :: [Enemy]
+draconum = concat
+  [ replicate 2 Enemy
+      { enemyName      = "Fire Dragon"
+      , enemyType      = Draconum
+      , enemyArmour    = 7
+      , enemyAbilities = Set.fromList [ Resists Physycal, Resists Fire ]
+      , enemyAttack    = AttcaksWith Fire 9
+      , enemyFameGain  = 8
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "High Dragon"
+      , enemyType      = Draconum
+      , enemyArmour    = 9
+      , enemyAbilities = Set.fromList [ Resists Fire, Resists Ice, Brutal ]
+      , enemyAttack    = AttcaksWith ColdFire 6
+      , enemyFameGain  = 9
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Ice Dragon"
+      , enemyType      = Draconum
+      , enemyArmour    = 7
+      , enemyAbilities = Set.fromList [ Resists Physycal, Resists Ice,
+                                                              Paralyzes]
+      , enemyAttack    = AttcaksWith Ice 6
+      , enemyFameGain  = 8
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Swap Dragon"
+      , enemyType      = Draconum
+      , enemyArmour    = 9
+      , enemyAbilities = Set.fromList [ Swift, Poisons ]
+      , enemyAttack    = AttcaksWith Physycal 5
+      , enemyFameGain  = 7
+      }
+  ]
+
+
+citizens :: [Enemy]
+citizens = concat
+  [ replicate 2 Enemy
+      { enemyName      = "Altem Guardsmen"
+      , enemyType      = Citizen
+      , enemyArmour    = 7
+      , enemyAbilities = Set.fromList [ Fortified, Resists Fire, Resists Ice,
+                                        Resists Physycal ]
+      , enemyAttack    = AttcaksWith Physycal 6
+      , enemyFameGain  = 8
+      }
+
+  , replicate 2 Enemy
+      { enemyName      = "Altem Mages"
+      , enemyType      = Citizen
+      , enemyArmour    = 8
+      , enemyAbilities = Set.fromList [ Fortified, Resists Physycal,
+                                                            Brutal, Poisons ]
+      , enemyAttack    = AttcaksWith ColdFire 4
+      , enemyFameGain  = 8
+      }
+
+  , replicate 3 Enemy
+      { enemyName      = "Freezers"
+      , enemyType      = Citizen
+      , enemyArmour    = 7
+      , enemyAbilities = Set.fromList [ Resists Fire, Swift, Paralyzes ]
+      , enemyAttack    = AttcaksWith Ice 3
+      , enemyFameGain  = 7
+      }
+
+  , replicate 3 Enemy
+      { enemyName      = "Gunners"
+      , enemyType      = Citizen
+      , enemyArmour    = 6
+      , enemyAbilities = Set.fromList [ Resists Ice, Brutal ]
+      , enemyAttack    = AttcaksWith Fire 6
+      , enemyFameGain  = 7
+      }
+
+  ]
