b3ce41c2452efecc649a90fa2880b80e3e087984
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-01-17 13:48:47 -0800

    Export types to JSON

---
+++ MageKnight/JSON.hs
@@ -0,0 +1,171 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards, FlexibleInstances #-}
+module MageKnight.JSON where
+
+import MageKnight.Game
+import MageKnight.Common
+import MageKnight.Cards
+import MageKnight.Terrain hiding (tile)
+import MageKnight.GameTile
+import MageKnight.HexContent
+import MageKnight.Enemies
+import MageKnight.Player
+import MageKnight.Ruins hiding (ruins)
+import MageKnight.Bag
+
+import           Data.Text(Text)
+import qualified Data.Aeson as JS
+import qualified Data.Aeson.Types as JS
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Vector as Vector
+
+class Export a where
+  toJS :: a -> JS.Value
+
+(.=) :: Export a => Text -> a -> JS.Pair
+x .= y = x JS..= toJS y
+
+instance Export Int where
+  toJS n = JS.Number (fromIntegral n)
+
+instance Export Text where
+  toJS xs = JS.String xs
+
+instance Export JS.Value where
+  toJS x = x
+
+instance Export a => Export (Maybe a) where
+  toJS x = case x of
+             Nothing -> JS.Null
+             Just a  -> toJS a
+
+instance Export a => Export [a] where
+  toJS xs = JS.Array (Vector.fromList (map toJS xs))
+
+--------------------------------------------------------------------------------
+
+
+instance Export Time where
+  toJS t =
+    case t of
+      Day   -> JS.String "day"
+      Night -> JS.String "night"
+
+instance Export HexContent where
+  toJS HexContent { .. } =
+    JS.object [ "shields" .= reverse hexShields
+              , "enemies" .= fmap enemy      (bagToList hexEnemies)
+              , "players" .= fmap playerName (Set.toList hexPlayers)
+              , "ruins"   .= fmap ruins      hexRuins
+              ]
+      where
+      jsEnemy ty name = JS.object [ "type" .= toJS ty, "name" .= name ]
+
+      enemy (v,e) = jsEnemy (enemyType e)
+                  $ case v of
+                      Hidden   -> "back"
+                      Revealed -> enemyName e
+
+      ruins (v,r) = case v of
+                      Hidden   -> "back"
+                      Revealed -> ruinsName r
+
+instance Export EnemyType where
+  toJS et = toJS (txt :: Text)
+    where
+    txt = case et of
+            Orc         -> "orc"
+            Guardian    -> "guardian"
+            Mage        -> "mage"
+            Underworld  -> "underworld"
+            Citizen     -> "citizens"
+            Draconum    -> "draconum"
+
+instance Export BasicMana where
+  toJS m = toJS (txt :: Text)
+    where
+    txt = case m of
+            White -> "white"
+            Red   -> "red"
+            Blue  -> "blue"
+            Green -> "green"
+
+instance Export Mana where
+  toJS m = case m of
+             Gold        -> toJS ("gold" :: Text)
+             Black       -> toJS ("black" :: Text)
+             BasicMana b -> toJS b
+
+instance Export MapShape where
+  toJS sh = case sh of
+              Wedge           -> JS.object [ "shape" .= ("wedge" :: Text) ]
+              OpenMap up down -> JS.object [ "shape" .= ("open" :: Text)
+                                           , "up"    .= up
+                                           , "down"  .= down
+                                           ]
+
+instance Export Dir where
+  toJS dir = toJS (txt :: Text)
+    where
+    txt = case dir of
+            NE -> "NE"
+            E  -> "E"
+            SE -> "SE"
+            SW -> "SW"
+            W  -> "W"
+            NW -> "NW"
+
+
+instance Export HexAddr where
+  toJS addr = case addr of
+                Center   -> toJS ("C" :: Text)
+                Border b -> toJS b
+
+
+instance Export TileType where
+  toJS t = toJS (txt :: Text)
+    where
+    txt = case t of
+            BasicTile -> "basic"
+            CoreTile  -> "core"
+
+
+instance Export Tile where
+  toJS Tile { .. } = JS.object [ "name" .= tileName, "type" .= tileType ]
+
+
+instance Export GameTile where
+  toJS GameTile { .. } =
+    JS.object [ "tile"    .= gameTile
+              , "content" .= map export (Map.toList gameTileContent)
+              ]
+    where
+    export (l,c) = JS.object [ "location" .= l, "content"  .= c ]
+
+instance Export Card where
+  toJS Card { .. } = toJS cardName
+
+instance Export Offers where
+  toJS Offers { .. } =
+    JS.object
+      [ "advancedActions" .= offering advancedActionOffer
+      , "spells"          .= offering spellOffer
+      , "units"           .= offering unitOffer
+      ]
+
+instance Export Game where
+  toJS Game { .. } =
+    JS.object
+      [ "time"      .= gameTime
+      , "source"    .= bagToList theSource
+      , "offers"    .= offers
+      , "mapShape"  .= mapShape
+      , "map"       .= map tile (Map.toList theMap)
+      , "nextTiles" .= map tileType (unexploredTiles ++ backupTiles)
+      ]
+
+    where
+    tile ((x,y),t) = JS.object [ "x" .= x, "y" .= y, "tile" .= t ]
+
+
+
