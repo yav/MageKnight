032bd2708802109d2523481905200bc2f7de0dec
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-02-16 22:04:40 -0800

    Basic combat rules

--- MageKnight/Combat.hs
+++ MageKnight/Combat.hs
@@ -1,15 +1,32 @@
+{-# LANGUAGE Safe, RecordWildCards #-}
 module MageKnight.Combat where
 
+import MageKnight.Common
 import MageKnight.Enemies
+import MageKnight.Bag
 
+import           Data.Map (Map)
+import qualified Data.Map as Map
 import           Data.Set (Set)
 import qualified Data.Set as Set
+import           Data.Either (partitionEithers)
+import           Data.Foldable (any, foldr)
+import           Prelude hiding (any, foldr)
 
 data ActiveEnemy = ActiveEnemy
   { enemyId       :: Int    -- ^ Used to distinguish enemies during a combat
   , enemyFortLoc  :: Bool   -- ^ In a fortified location
   , enemyStats    :: Enemy  -- ^ Current enemy stats
   }
+
+enemyHasAbility :: EnemyAbility -> ActiveEnemy -> Bool
+enemyHasAbility x = Set.member x . enemyAbilities . enemyStats
+
+enemyResists :: Element -> ActiveEnemy -> Bool
+enemyResists ColdFire e = enemyHasAbility (Resists Ice) e &&
+                          enemyHasAbility (Resists Fire) e
+enemyResists x e        = enemyHasAbility (Resists x) e
+
 
 instance Eq ActiveEnemy where
   x == y = enemyId x == enemyId y
@@ -23,28 +40,107 @@ data CombatPhase = RangedAttackPhase (Set ActiveEnemy)
                  | BlockingPhase (Maybe ActiveEnemy) -- Currently blocking
                                  (Set ActiveEnemy)   -- Blocked
 
+                 | AssignDamage Int
+                   -- ^ This is how much damage needs to get assigned.
+
                  | AttackPhase (Set ActiveEnemy)
                    -- ^ Enemies that we are attacking at the moment
 
-data Enemies = Enemies
-  { activeEnemies   :: Set ActiveEnemy
+-- XXX: Some cards make enemies not attack;  we need to rememebr that
+-- XXX: Some crads decrease armor when used to block
+data Battle = Battle
+  { activeEnemies   :: Map Int ActiveEnemy
   , defeatedEnemies :: Set ActiveEnemy
   , combatPhase     :: CombatPhase
   }
 
-startCombat :: [(Enemy,Bool)] -> Enemies
-startCombat foes = Enemies
-  { activeEnemies   = Set.fromList (zipWith activate [0 .. ] foes)
+newBattle :: [(Enemy,Bool)] -> Battle
+newBattle foes = Battle
+  { activeEnemies   = Map.fromList (zipWith activate [0 .. ] foes)
   , defeatedEnemies = Set.empty
   , combatPhase     = RangedAttackPhase Set.empty
   }
   where
-  activate n (e,inFort) = ActiveEnemy { enemyId      = n
-                                      , enemyFortLoc = inFort
-                                      , enemyStats   = e
-                                      }
+  activate n (e,inFort) = (n, ActiveEnemy { enemyId      = n
+                                          , enemyFortLoc = inFort
+                                          , enemyStats   = e
+                                          })
 
 
+
+-- | Pick an enemy to attack or block, depending on the current
+-- phase of the battle.
+selectTarget :: Int -> Battle -> Maybe Battle
+selectTarget n Battle { .. } =
+  case ( combatPhase
+       , Map.updateLookupWithKey (\_ _ -> Nothing) n activeEnemies
+       ) of
+
+    (RangedAttackPhase es, (Just e, m1)) ->
+      Just Battle { activeEnemies = m1
+                  , combatPhase = RangedAttackPhase (Set.insert e es)
+                  , ..
+                  }
+
+    (BlockingPhase Nothing bs, (Just e, m1)) ->
+      Just Battle { activeEnemies = m1
+                  , combatPhase = BlockingPhase (Just e) bs
+                  , ..
+                  }
+
+    (AttackPhase es, (Just e, m1)) ->
+      Just Battle { activeEnemies = m1
+                  , combatPhase = AttackPhase (Set.insert e es)
+                  , ..
+                  }
+    _ -> Nothing
+
+
+-- | Check if the attacks are sufficient to defeat the set of enemies.
+successfulRangedAttack :: Bag Element {-^ Ranged -} ->
+                          Bag Element {-^ Siege  -} ->
+                          Set ActiveEnemy -> Bool
+successfulRangedAttack rangedAttack siegeAttack enemies =
+  successfulAttack attackTypes enemies
+  where
+  inFort    = any enemyFortLoc enemies
+  fortified = any (enemyHasAbility Fortified) enemies
+
+  attackTypes
+    | inFort    = if fortified then bagEmpty else siegeAttack
+    | fortified = siegeAttack
+    | otherwise = bagUnion rangedAttack siegeAttack
+
+-- | Check if the attacks are sufficient to defeat the set of enemies.
+successfulAttack :: Bag Element -> Set ActiveEnemy -> Bool
+successfulAttack attackTypes enemies =
+  totalAttack >= foldr addArmor 0 enemies
+  where
+  addArmor e tot          = enemyArmor (enemyStats e) + tot
+  resists x               = any (enemyResists x) enemies
+
+  classify (el,amt)       = if resists el then Left amt else Right amt
+  (inefficient,efficient) = partitionEithers $ map classify
+                                             $ bagToListGrouped attackTypes
+  totalAttack             = sum (div (sum inefficient) 2 : efficient)
+
+
+-- | Check if the given amounts of block are suffivient to block an enemy.
+successfulBlock :: Bag Element -> ActiveEnemy -> Bool
+successfulBlock blocks enemy =
+  case enemyAttack (enemyStats enemy) of
+    AttcaksWith ty amt -> sum (div (sum inefficient) 2 : efficient) >= amt
+      where
+      (inefficient,efficient) = partitionEithers $ map classify
+                                                 $ bagToListGrouped blocks
+      classify (bt,a) = if isEfficient bt then Right a else Left a
+      isEfficient = case ty of
+                      Physycal -> \_ -> True
+                      Fire     -> \x -> x == Ice  || x == ColdFire
+                      Ice      -> \x -> x == Fire || x == ColdFire
+                      ColdFire -> (== ColdFire)
+
+    Summoner -> False -- XXX: Should not happen.
 
 
 
--- MageKnight/Enemies.hs
+++ MageKnight/Enemies.hs
@@ -15,7 +15,7 @@ type EnemyPool = Map EnemyType (ResourceQ Enemy)
 data EnemyAbility =
 
     Fortified
-  | Resists Element
+  | Resists Element -- ^ "ColdFire" does not appear as a resiatnce
   | Swift
   | Brutal
   | Poisons
@@ -35,7 +35,7 @@ allEnemyTypes = [ minBound .. maxBound ]
 data Enemy = Enemy
   { enemyName       :: Text
   , enemyType       :: EnemyType
-  , enemyArmour     :: Int
+  , enemyArmor     :: Int
   , enemyAttack     :: EnemyAttack
   , enemyFameGain   :: Int
   , enemyAbilities  :: Set EnemyAbility
@@ -74,7 +74,7 @@ orcs = concat
   [ replicate 2 Enemy
       { enemyName      = "Prowlers"
       , enemyType      = Orc
-      , enemyArmour    = 3
+      , enemyArmor    = 3
       , enemyAttack    = AttcaksWith Physycal 4
       , enemyFameGain  = 2
       , enemyAbilities = Set.empty
@@ -83,7 +83,7 @@ orcs = concat
   , replicate 2 Enemy
       { enemyName      = "Diggers"
       , enemyType      = Orc
-      , enemyArmour    = 3
+      , enemyArmor    = 3
       , enemyAbilities = Set.fromList [ Fortified ]
       , enemyAttack    = AttcaksWith Physycal 3
       , enemyFameGain  = 2
@@ -92,7 +92,7 @@ orcs = concat
   , replicate 2 Enemy
       { enemyName      = "Cursed Hags"
       , enemyType      = Orc
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAttack    = AttcaksWith Physycal 3
       , enemyAbilities = Set.fromList [ Poisons ]
       , enemyFameGain  = 3
@@ -101,7 +101,7 @@ orcs = concat
   , replicate 2 Enemy
       { enemyName      = "Wolf Riders"
       , enemyType      = Orc
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAttack    = AttcaksWith Physycal 3
       , enemyAbilities = Set.fromList [ Swift ]
       , enemyFameGain  = 3
@@ -110,7 +110,7 @@ orcs = concat
   , replicate 2 Enemy
       { enemyName      = "Ironclads"
       , enemyType      = Orc
-      , enemyArmour    = 3
+      , enemyArmor    = 3
       , enemyAttack    = AttcaksWith Physycal 4
       , enemyAbilities = Set.fromList [ Brutal ]
       , enemyFameGain  = 4
@@ -119,7 +119,7 @@ orcs = concat
   , replicate 2 Enemy
       { enemyName      = "Orc Summoners"
       , enemyType      = Orc
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAttack    = Summoner
       , enemyFameGain  = 4
       , enemyAbilities = Set.empty
@@ -133,7 +133,7 @@ keep = concat
   [ replicate 3 Enemy
       { enemyName      = "Crossbowmen"
       , enemyType      = Guardian
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAttack    = AttcaksWith Physycal 4
       , enemyAbilities = Set.fromList [ Swift ]
       , enemyFameGain  = 3
@@ -142,7 +142,7 @@ keep = concat
   , replicate 2 Enemy
       { enemyName      = "Golems"
       , enemyType      = Guardian
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.fromList [ Resists Physycal ]
       , enemyAttack    = AttcaksWith Physycal 2
       , enemyFameGain  = 4
@@ -151,7 +151,7 @@ keep = concat
   , replicate 3 Enemy
       { enemyName      = "Guardsmen"
       , enemyType      = Guardian
-      , enemyArmour    = 7
+      , enemyArmor    = 7
       , enemyAbilities = Set.fromList [ Fortified ]
       , enemyAttack    = AttcaksWith Physycal 3
       , enemyFameGain  = 3
@@ -160,7 +160,7 @@ keep = concat
   , replicate 2 Enemy
       { enemyName      = "Swordsmen"
       , enemyType      = Guardian
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.empty
       , enemyAttack    = AttcaksWith Physycal 6
       , enemyFameGain  = 4
@@ -174,7 +174,7 @@ dungeon = concat
   [ replicate 2 Enemy
       { enemyName      = "Crypt Worm"
       , enemyType      = Underworld
-      , enemyArmour    = 6
+      , enemyArmor    = 6
       , enemyAbilities = Set.fromList [ Fortified ]
       , enemyAttack    = AttcaksWith Physycal 6
       , enemyFameGain  = 5
@@ -183,7 +183,7 @@ dungeon = concat
   , replicate 2 Enemy
       { enemyName      = "Gargoyle"
       , enemyType      = Underworld
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAbilities = Set.fromList [ Resists Physycal ]
       , enemyAttack    = AttcaksWith Physycal 5
       , enemyFameGain  = 4
@@ -192,7 +192,7 @@ dungeon = concat
   , replicate 2 Enemy
       { enemyName      = "Medusa"
       , enemyType      = Underworld
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAbilities = Set.fromList [ Paralyzes ]
       , enemyAttack    = AttcaksWith Physycal 6
       , enemyFameGain  = 5
@@ -201,7 +201,7 @@ dungeon = concat
   , replicate 2 Enemy
       { enemyName      = "Minotaur"
       , enemyType      = Underworld
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.fromList [ Brutal ]
       , enemyAttack    = AttcaksWith Physycal 5
       , enemyFameGain  = 4
@@ -210,7 +210,7 @@ dungeon = concat
   , replicate 2 Enemy
       { enemyName      = "Werewolf"
       , enemyType      = Underworld
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.fromList [ Swift ]
       , enemyAttack    = AttcaksWith Physycal 7
       , enemyFameGain  = 5
@@ -224,7 +224,7 @@ magical = concat
   [ replicate 2 Enemy
       { enemyName      = "Monks"
       , enemyType      = Mage
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.fromList [ Poisons ]
       , enemyAttack    = AttcaksWith Physycal 5
       , enemyFameGain  = 4
@@ -233,7 +233,7 @@ magical = concat
   , replicate 2 Enemy
       { enemyName      = "Illusionists"
       , enemyType      = Mage
-      , enemyArmour    = 3
+      , enemyArmor    = 3
       , enemyAbilities = Set.fromList [ Resists Physycal ]
       , enemyAttack    = Summoner
       , enemyFameGain  = 4
@@ -242,7 +242,7 @@ magical = concat
   , replicate 2 Enemy
       { enemyName      = "Ice Mages"
       , enemyType      = Mage
-      , enemyArmour    = 6
+      , enemyArmor    = 6
       , enemyAbilities = Set.fromList [ Resists Ice ]
       , enemyAttack    = AttcaksWith Ice 5
       , enemyFameGain  = 5
@@ -251,7 +251,7 @@ magical = concat
   , replicate 1 Enemy
       { enemyName      = "Ice Golems"
       , enemyType      = Mage
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAbilities = Set.fromList [ Resists Physycal, Resists Ice,
                                                                 Paralyzes ]
       , enemyAttack    = AttcaksWith Ice 2
@@ -261,7 +261,7 @@ magical = concat
   , replicate 2 Enemy
       { enemyName      = "Fire Mages"
       , enemyType      = Mage
-      , enemyArmour    = 5
+      , enemyArmor    = 5
       , enemyAbilities = Set.fromList [ Resists Fire ]
       , enemyAttack    = AttcaksWith Fire 6
       , enemyFameGain  = 5
@@ -270,7 +270,7 @@ magical = concat
   , replicate 1 Enemy
       { enemyName      = "Fire Golems"
       , enemyType      = Mage
-      , enemyArmour    = 4
+      , enemyArmor    = 4
       , enemyAbilities = Set.fromList [ Resists Physycal, Resists Fire, Brutal ]
       , enemyAttack    = AttcaksWith Fire 3
       , enemyFameGain  = 5
@@ -284,7 +284,7 @@ draconum = concat
   [ replicate 2 Enemy
       { enemyName      = "Fire Dragon"
       , enemyType      = Draconum
-      , enemyArmour    = 7
+      , enemyArmor    = 7
       , enemyAbilities = Set.fromList [ Resists Physycal, Resists Fire ]
       , enemyAttack    = AttcaksWith Fire 9
       , enemyFameGain  = 8
@@ -293,7 +293,7 @@ draconum = concat
   , replicate 2 Enemy
       { enemyName      = "High Dragon"
       , enemyType      = Draconum
-      , enemyArmour    = 9
+      , enemyArmor    = 9
       , enemyAbilities = Set.fromList [ Resists Fire, Resists Ice, Brutal ]
       , enemyAttack    = AttcaksWith ColdFire 6
       , enemyFameGain  = 9
@@ -302,7 +302,7 @@ draconum = concat
   , replicate 2 Enemy
       { enemyName      = "Ice Dragon"
       , enemyType      = Draconum
-      , enemyArmour    = 7
+      , enemyArmor    = 7
       , enemyAbilities = Set.fromList [ Resists Physycal, Resists Ice,
                                                               Paralyzes]
       , enemyAttack    = AttcaksWith Ice 6
@@ -312,7 +312,7 @@ draconum = concat
   , replicate 2 Enemy
       { enemyName      = "Swamp Dragon"
       , enemyType      = Draconum
-      , enemyArmour    = 9
+      , enemyArmor    = 9
       , enemyAbilities = Set.fromList [ Swift, Poisons ]
       , enemyAttack    = AttcaksWith Physycal 5
       , enemyFameGain  = 7
@@ -325,7 +325,7 @@ citizens = concat
   [ replicate 2 Enemy
       { enemyName      = "Altem Guardsmen"
       , enemyType      = Citizen
-      , enemyArmour    = 7
+      , enemyArmor    = 7
       , enemyAbilities = Set.fromList [ Fortified, Resists Fire, Resists Ice,
                                         Resists Physycal ]
       , enemyAttack    = AttcaksWith Physycal 6
@@ -335,7 +335,7 @@ citizens = concat
   , replicate 2 Enemy
       { enemyName      = "Altem Mages"
       , enemyType      = Citizen
-      , enemyArmour    = 8
+      , enemyArmor    = 8
       , enemyAbilities = Set.fromList [ Fortified, Resists Physycal,
                                                             Brutal, Poisons ]
       , enemyAttack    = AttcaksWith ColdFire 4
@@ -345,7 +345,7 @@ citizens = concat
   , replicate 3 Enemy
       { enemyName      = "Freezers"
       , enemyType      = Citizen
-      , enemyArmour    = 7
+      , enemyArmor    = 7
       , enemyAbilities = Set.fromList [ Resists Fire, Swift, Paralyzes ]
       , enemyAttack    = AttcaksWith Ice 3
       , enemyFameGain  = 7
@@ -354,7 +354,7 @@ citizens = concat
   , replicate 3 Enemy
       { enemyName      = "Gunners"
       , enemyType      = Citizen
-      , enemyArmour    = 6
+      , enemyArmor    = 6
       , enemyAbilities = Set.fromList [ Resists Ice, Brutal ]
       , enemyAttack    = AttcaksWith Fire 6
       , enemyFameGain  = 7
