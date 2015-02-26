34d791695037874cc2ba3b972e48032687808c4a
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-02-25 22:36:30 -0800

    Drawing activated cards

--- MageKnight/Spell.hs
+++ MageKnight/Spell.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE Safe, OverloadedStrings #-}
 module MageKnight.Spell (deeds, interactiveSpell) where
 
+import MageKnight.Common
 import MageKnight.Deed
 
 -- XXX
 deeds :: [Deed]
-deeds = []
+deeds =
+  [ -- Just to test layout
+    spellDeed Red "Burning Shield" "Exploding Shield" [] []
+  ]
 
 interactiveSpell :: Deed -> Bool
 interactiveSpell d = deedName d `elem`
