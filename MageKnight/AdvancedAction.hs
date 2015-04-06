d7ad02e654112090ff273bd14a8418a59ae98c95
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-04-05 21:40:37 -0700

    Type in names of advanced actions

--- MageKnight/AdvancedAction.hs
+++ MageKnight/AdvancedAction.hs
@@ -31,6 +31,7 @@ blueDeeds =
 
 
   -- XXX: Magic Talent
+  , deed "Magic Talent" [] []
 
   , let gain n x y = [ManaToken (BasicMana x)] --> replicate n y
         table      = [ (Green, Movement)
@@ -60,11 +61,12 @@ greenDeeds =
       [ [] --> [ ManaCrystal Green ] ]
       [ [] --> replicate 3 (Attack Siege Physycal) ]
 
-  , deed "Ambush"
-      [] -- XXX
-      [] -- XXX
-
-
+  , deed "Ambush" [] []
+  , deed "In Need" [] []
+  , deed "Path Finding" [] []
+  , deed "Refreshing Walk" [] []
+  , deed "Regeneration" [] []
+  , deed "Training" [] []
   ]
   where
   deed = advancedActionDeed Green
@@ -72,7 +74,13 @@ greenDeeds =
 -- XXX: red
 redDeeds :: [Deed]
 redDeeds =
-  [
+  [ deed "Blood Rage" [] []
+  , deed "Blood Ritual" [] []
+  , deed "Decompose" [] []
+  , deed "Fire Bolt" [] []
+  , deed "Intimidate" [] []
+  , deed "Into the Heat" [] []
+  , deed "Maximal Effect" [] []
   ]
   where
   deed = advancedActionDeed Red
@@ -80,7 +88,13 @@ redDeeds =
 -- XXX: white
 whiteDeeds :: [Deed]
 whiteDeeds =
-  [
+  [ deed "Agility" [] []
+  , deed "Diplomacy" [] []
+  , deed "Heroic Tale" [] []
+  , deed "Learning" [] []
+  , deed "Mana Storm" [] []
+  , deed "Song of Wind" [] []
+  , deed "Swift Bolt" [] []
   ]
   where
   deed = advancedActionDeed White
