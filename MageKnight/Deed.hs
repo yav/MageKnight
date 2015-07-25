1c005088f7036f0925c7501035208fb61f4c5e9f
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-07-25 12:00:41 -0700

    Clean up rules, add support for conditionals based on time

--- MageKnight/Deed.hs
+++ MageKnight/Deed.hs
@@ -102,18 +102,18 @@ artifactDeed deedName deedBasic deedPower =
 deedRules :: Deed -> [Rule]
 deedRules Deed { .. } =
   case deedType of
-    Wound     -> []
-    Action c  -> sidewaysRules ++ basicRules        ++ actionPowerRules c
+    Wound            -> []
+    Action c         -> sidewaysRules ++ basicRules ++ actionPowerRules c
     AdvancedAction c -> sidewaysRules ++ basicRules ++ actionPowerRules c
-    Spell c   -> sidewaysRules ++ spellBasicRules c ++ spellPowerRules c
-    Artifact  -> sidewaysRules ++ basicRules        ++ artifactPowerRules
+    Spell c          -> sidewaysRules ++ spellBasicRules c ++ spellPowerRules c
+    Artifact         -> sidewaysRules ++ basicRules ++ artifactPowerRules
 
   where
   sidewaysRules =
-    [ "use for 1 movement" === [ADeed deedName] --> [Movement]
-    , "use for 1 influece" === [ADeed deedName] --> [Influence]
-    , "use for 1 attack"   === [ADeed deedName] --> [Attack Melee Physycal]
-    , "use for 1 block"    === [ADeed deedName] --> [Block Physycal]
+    [ "use for 1 movement" === [DeedInHand deedName] --> [Movement]
+    , "use for 1 influece" === [DeedInHand deedName] --> [Influence]
+    , "use for 1 attack"   === [DeedInHand deedName] --> [Attack Melee Physycal]
+    , "use for 1 block"    === [DeedInHand deedName] --> [Block Physycal]
     ]
 
   powerRuleName = case deedNamePower of
@@ -122,13 +122,13 @@ deedRules Deed { .. } =
 
   basicRules =
     [ Rule { ruleName = deedName
-           , ruleIn   = bagAdd 1 (ADeed deedName) ruleIn
+           , ruleIn   = bagAdd 1 (DeedInHand deedName) ruleIn
            , ..
            } | Rule { .. } <- deedBasic ]
 
   actionPowerRules c =
     [ Rule { ruleName = powerRuleName
-           , ruleIn   = bagAdd 1 (ADeed deedName)
+           , ruleIn   = bagAdd 1 (DeedInHand deedName)
                       $ bagAdd 1 (ManaToken (BasicMana c)) ruleIn
            , ..
            } | Rule { .. } <- deedPower
@@ -139,13 +139,13 @@ deedRules Deed { .. } =
         | Rule { .. } <- basicRules ]
 
   spellPowerRules c =
-    [ Rule { ruleIn = bagAdd 1 (ManaToken Black) ruleIn, .. }
+    [ onlyDuring Night Rule { ruleIn  = bagAdd 1 (ManaToken Black) ruleIn, .. }
         | Rule { .. } <- actionPowerRules c
     ]
 
   artifactPowerRules =
     [ Rule { ruleName = powerRuleName
-           , ruleOut  = bagAdd 1 (ThrowAway deedName) ruleOut
+           , ruleOut  = bagAdd 1 (DeedDestroyed deedName) ruleOut
            , ..
            } | Rule { .. } <- deedPower
     ]
--- MageKnight/Rule.hs
+++ MageKnight/Rule.hs
@@ -10,6 +10,77 @@ import           Control.Monad (foldM)
 import           Text.PrettyPrint
 
 
+
+--------------------------------------------------------------------------------
+
+
+
+data Rule = Rule
+  { ruleName  :: Text
+  , ruleIn    :: Bag Resource   -- ^ We consume these
+  , ruleOut   :: Bag Resource   -- ^ We produce these
+  }
+
+useRule :: Rule -> Bag Resource -> Maybe (Bag Resource)
+useRule Rule { .. } rAvail =
+  bagUnion ruleOut `fmap` foldM rm rAvail (bagToListGrouped ruleIn)
+  where
+  rm rs (r,q) = bagRemove q r rs
+
+ppRule :: Rule -> Doc
+ppRule Rule { .. } = vcat [ text (Text.unpack ruleName) <> text ":"
+                          , nest 2 (ppResources ruleIn)
+                          , text "-->"
+                          , nest 2 (ppResources ruleOut)
+                          ]
+
+
+-- Rule DSL --------------------------------------------------------------------
+
+infix 1 ===
+infix 2 -->
+
+-- | Define an anonymous rule.
+(-->) :: [Resource] -> [Resource] -> Rule
+rIn --> rOut = Rule { ruleName = ""
+                    , ruleIn   = bagFromList rIn
+                    , ruleOut  = bagFromList rOut
+                    }
+
+-- | Set the name for a rule.
+(===) :: Text -> Rule -> Rule
+name === rule = rule { ruleName = name }
+
+onlyDuring :: Time -> Rule -> Rule
+onlyDuring t Rule { .. } =
+  Rule { ruleIn  = bagAdd 1 (TimeIs t) ruleIn
+       , ruleOut = bagAdd 1 (TimeIs t) ruleOut
+       , ..
+       }
+
+
+rules :: [ Rule ]
+rules =
+
+  [ onlyDuring Day
+  $ Text.pack ("gold -> " ++ show (ppBasicMana b)) ===
+    [ ManaToken Gold ] --> [ ManaToken (BasicMana b) ]
+  | b <- anyBasicMana
+  ]
+  ++
+  [ Text.pack ("take " ++ show (ppMana m) ++ " die") ===
+    [ ManaDie, ManaSource m ] --> [ ManaToken m ]
+  | m <- anyMana
+  ]
+  ++
+  [ Text.pack ("use " ++ show (ppBasicMana b) ++ " crystal") ===
+    [ ManaCrystal b ] --> [ ManaToken (BasicMana b), UsedCrystal b ]
+  | b <- anyBasicMana
+  ]
+
+
+--------------------------------------------------------------------------------
+
 data TerrainCostChange =
     DecreaseTo Int      -- ^ Decrease to the given value
   | DecreaseBy Int Int  -- ^ Decrease by amount, to minimum
@@ -20,19 +91,28 @@ data Resource =
 
     ManaToken Mana
   | ManaCrystal BasicMana
+  | UsedCrystal BasicMana       -- ^ A used crystal of this type
 
-  | ManaSource Mana
-  | ManaDie
+  | ManaSource Mana             -- ^ Mana in the source
+  | ManaDie                     -- ^ Access to the source
 
   | Movement
   | Influence
   | Attack AttackType Element
   | Block Element
-
+  | Fame
   | Healing
-  | ReadyUnit Int   -- ^ Ready a unit of this level
 
-  | ADeed DeedName
+  | DeedInHand DeedName         -- ^ A specific card
+  | DeedDestroyed DeedName      -- ^ This is out of the game
+  | DeedDiscarded DeedName      -- ^ This was discarded
+
+  | TimeIs Time                 -- ^ Time of day
+
+{-
+  | ReadyUnit Int               -- ^ Ready a unit of this level
+
+
 
   | Blocking EnemyName
 
@@ -49,6 +129,7 @@ data Resource =
   | ThrowAway DeedName
   | ToDeedDeckBottom DeedName
   | ToDeedDeckTop DeedName
+  -}
     deriving (Eq,Ord,Show)
 
 
@@ -57,24 +138,33 @@ data Resource =
 ppResource :: Resource -> Doc
 ppResource resource =
   case resource of
-    ManaToken m -> ppMana m <+> text "token"
+    ManaToken m   -> ppMana m <+> text "token"
     ManaCrystal m -> ppBasicMana m <+> text "crystal"
-    RegainUsedCrystals -> text "regain unused crystals"
-    ManaSource m -> ppMana m <+> text "source"
-    ManaSourceFixed m -> ppMana m <+> text "source (fixed)"
-    ManaDie -> text "mana die"
+    UsedCrystal m -> text "used" <+> ppBasicMana m <+> text "crystal"
+    ManaSource m  -> ppMana m <+> text "in the source"
+    ManaDie       -> text "use of source"
 
-    Movement -> text "movement"
-    Influence -> text "influence"
+    Movement      -> text "movement"
+    Influence     -> text "influence"
     Attack at el -> elDoc <+> tyDoc <+> text "attack"
       where elDoc = ppElement el
             tyDoc = case at of
                       Melee  -> empty
                       Ranged -> text "ranged"
                       Siege  -> text "siege"
-    Block e  -> ppElement e
+    Block e  -> ppElement e <+> text "block"
     Healing  -> text "healing"
-    ADeed x  -> text (show x) <+> text "card"
+    Fame     -> text "fame"
+
+    DeedInHand x    -> text (show x) <+> text "card"
+    DeedDestroyed x -> text (show x) <+> text "was destroyed"
+    DeedDiscarded x -> text (show x) <+> text "was discarded"
+
+    TimeIs t        -> text "it is" <+> ppTime t
+
+    {-
+    ManaSourceFixed m -> ppMana m <+> text "source (fixed)"
+    RegainUsedCrystals -> text "regain unused crystals"
 
     -- XXX
     ChangeTerrainCost t c -> text "change terrain cost"
@@ -92,71 +182,12 @@ ppResource resource =
     ToDeedDeckTop x -> text "place" <+> text (show x) <+>
                        text "at the top of the deed deck"
 
-
+    -}
 
 ppResources :: Bag Resource -> Doc
 ppResources = vcat . map ppEntry . bagToListGrouped
   where ppEntry (r,x) = int x <+> ppResource r
 
-
-
-
-
-
-
-
-data Rule = Rule
-  { ruleName  :: Text
-  , ruleIn    :: Bag Resource
-  , ruleOut   :: Bag Resource
-  }
-
-useRule :: Rule -> Bag Resource -> Maybe (Bag Resource)
-useRule Rule { .. } rAvail =
-  bagUnion ruleOut `fmap` foldM rm rAvail (bagToListGrouped ruleIn)
-  where
-  rm rs (r,q) = bagRemove q r rs
-
-ppRule :: Rule -> Doc
-ppRule Rule { .. } = vcat [ text (Text.unpack ruleName) <> text ":"
-                          , nest 2 (ppResources ruleIn)
-                          , text "-->"
-                          , nest 2 (ppResources ruleOut)
-                          ]
-
-
-
-infix 1 ===
-infix 2 -->
-
-(-->) :: [Resource] -> [Resource] -> Rule
-rIn --> rOut = Rule { ruleName = ""
-                    , ruleIn   = bagFromList rIn
-                    , ruleOut  = bagFromList rOut
-                    }
-
-(===) :: Text -> Rule -> Rule
-name === rule = rule { ruleName = name }
-
-
-rules :: [ Rule ]
-rules =
-
-  -- XXX: Only during the day
-  [ Text.pack ("gold -> " ++ show (ppBasicMana b)) ===
-    [ ManaToken Gold ] --> [ ManaToken (BasicMana b) ]
-  | b <- anyBasicMana
-  ]
-  ++
-  [ Text.pack ("take " ++ show (ppMana m) ++ " die") ===
-    [ ManaDie, ManaSource m ] --> [ ManaToken m ]
-  | m <- anyMana
-  ]
-  ++
-  [ Text.pack ("use " ++ show (ppBasicMana b) ++ " crystal") ===
-    [ ManaCrystal b ] --> [ ManaToken (BasicMana b) ]
-  | b <- anyBasicMana
-  ]
 
 
 
