19cc7fba358ebd30e044c508a92d30d592af0d8c
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2015-12-05 12:24:14 -0800

    This is not a functional module, but it remembers what still needs to be done.

--- exes/Main.hs
+++ exes/Main.hs
@@ -2,19 +2,20 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import MageKnight.Common
-import MageKnight.Offers as Offers
-import MageKnight.Deed
-import MageKnight.Units
-import MageKnight.Terrain
-import MageKnight.JSON
-import MageKnight.Game
-import MageKnight.Land(getFeatureAt, getRevealedEnemiesAt)
-import MageKnight.Enemies
-import MageKnight.Player as Player
-import MageKnight.DeedDecks(findDeed)
-import MageKnight.Attr
-import MageKnight.Perhaps
+import Common
+import Offers as Offers
+import Deed
+import Units
+import Terrain
+import Game
+import Land(getFeatureAt, getRevealedEnemiesAt)
+import Enemies
+import Player as Player
+import DeedDecks(findDeed)
+
+import Util.Perhaps
+import Util.JSON
+import Util.Snap
 
 import           Snap.Http.Server (quickHttpServe)
 import           Snap.Core (Snap)
@@ -107,260 +108,15 @@ type Act = IORef SnapState -> Snap ()
 
 --------------------------------------------------------------------------------
 
-badInput :: ByteString -> Snap a
-badInput msg =
-  Snap.finishWith (Snap.setResponseStatus 400 msg Snap.emptyResponse)
-
-notFound :: Snap a
-notFound = Snap.finishWith (Snap.setResponseStatus 404 "Not Found"
-                                                      Snap.emptyResponse)
-
 --------------------------------------------------------------------------------
 
-requiredParam :: ByteString -> Snap ByteString
-requiredParam p =
-  do mb <- Snap.getParam p
-     case mb of
-       Just x  -> return x
-       Nothing -> badInput ("Missing parameter: " `BS.append` p)
-
-textParam :: ByteString -> Snap Text
-textParam p = decodeUtf8 `fmap` requiredParam p
-
-intParam :: ByteString -> Snap Int
-intParam p =
-  do txt <- textParam p
-     let (neg,numTxt) = case Text.uncons txt of
-                          Just ('-',t) -> (negate, t)
-                          _            -> (id, txt)
-     case decimal numTxt of
-       Right (a,t) | Text.null t -> return (neg a)
-       _ -> badInput ("Malformed integer parameter: " `BS.append` p)
-
-natParam :: ByteString -> Snap Int
-natParam p =
-  do txt <- textParam p
-     case decimal txt of
-       Right (a,t) | Text.null t -> return a
-       _ -> badInput ("Malformed natural parameter: " `BS.append` p)
-
-
-
-boolParam :: ByteString -> Snap Bool
-boolParam p =
-  do bs <- requiredParam p
-     case bs of
-       "true"  -> return True
-       "false" -> return False
-       _       -> badInput ("Malformed boolean parameter: " `BS.append` p)
-
-addrParam :: Snap Addr
-addrParam =
-  do x <- intParam  "tile_x"
-     y <- intParam  "tile_y"
-     h <- textParam "hex"
-     let ok d = return (hexAddr d)
-     lo <- case h of
-             "NW" -> ok NW
-             "NE" -> ok NE
-             "W"  -> ok W
-             "C"  -> ok Center
-             "E"  -> ok E
-             "SW" -> ok SW
-             "SE" -> ok SE
-             _    -> badInput "Malformed parameter: hex"
-     return Addr { addrGlobal = (x,y), addrLocal = lo }
-
-deedParam :: ByteString -> Snap Deed
-deedParam pname =
-  do x <- textParam pname
-     case findDeed x of
-       Just d  -> return d
-       Nothing -> badInput (BS.append "Invalid deed parameter: " pname)
-
-unitParam :: ByteString -> Snap Unit
-unitParam pname =
-  do x <- textParam pname
-     case findUnit x of
-       Just d  -> return d
-       Nothing -> badInput (BS.append "Invalid unit parameter: " pname)
-
-basicManaParam :: ByteString -> Snap BasicMana
-basicManaParam pname =
-  do x <- textParam pname
-     case x of
-       "red"    -> return Red
-       "green"  -> return Green
-       "blue"   -> return Blue
-       "white"  -> return White
-       _        -> badInput (BS.append "Inavlid basic mana: " pname)
-
-manaParam :: ByteString -> Snap Mana
-manaParam pname =
-  do x <- textParam pname
-     case x of
-       "red"    -> return (BasicMana Red)
-       "green"  -> return (BasicMana Green)
-       "blue"   -> return (BasicMana Blue)
-       "white"  -> return (BasicMana White)
-       "gold"   -> return Gold
-       "black"  -> return Black
-       _        -> badInput (BS.append "Inavlid mana: " pname)
-
-
-
-sendJSON :: Export a => a -> Snap ()
-sendJSON a =
-  do Snap.modifyResponse (Snap.setHeader "content-type" "application/json")
-     Snap.writeLBS (jsonBytes a)
 
 
 --------------------------------------------------------------------------------
 
 
-nameToPath :: Text -> String
-nameToPath = Text.unpack . Text.map cvt
-  where
-  cvt c | isAscii c && isAlphaNum c = toLower c
-  cvt _                             = '_'
-
-deedPath :: Deed -> FilePath
-deedPath d = "ui" </> deedUrl d
-
-deedUrl :: Deed -> FilePath
-deedUrl Deed { .. } = dir </> nameToPath deedName <.> "png"
-  where
-  dir = case deedType of
-          Wound            -> pref
-          Action c         -> pref </> "basic_actions" </> color c
-          AdvancedAction c -> pref </> "advanced_actions" </> color c
-          Spell c          -> pref </> "spells" </> color c
-          Artifact         -> pref </> "artifacts"
-  pref = "img" </> "cards"
-  color c = case c of
-              Red   -> "red"
-              Blue  -> "blue"
-              Green -> "green"
-              White -> "white"
-
-unitPath :: Unit -> FilePath
-unitPath u = "ui" </> unitUrl u
-
-unitUrl :: Unit -> FilePath
-unitUrl Unit { .. } = "img" </> "units" </> ty </> name <.> "png"
-  where ty = case unitType of
-               RegularUnit -> "regular"
-               EliteUnit   -> "elite"
-        name = nameToPath unitName
-
-
-
-featureHelpUrl :: Feature -> Maybe FilePath
-featureHelpUrl f =
-  case f of
-    MagicalGlade    -> img "magical_glade"
-    Mine _          -> img "mines"
-    Village         -> img "village"
-    Monastery       -> img "monastery"
-    Keep            -> img "keep"
-    MageTower       -> img "mage_tower"
-    Dungeon         -> img "dungeon"
-    Tomb            -> img "tomb"
-    MonsterDen      -> img "monster_den"
-    SpawningGrounds -> img "spawning_grounds"
-    AncientRuins    -> img "ancient_ruins"
-    RampagingEnemy e ->
-      case e of
-        Orc      -> img "marauding_orcs"
-        Draconum -> img "draconum"
-        _        -> Nothing
-
-  where
-  img x = Just ("img" </> "manual" </> "features" </> x <.> "png")
-
-getBuildingHelpUrls :: (Terrain, Maybe Feature) -> [FilePath]
-getBuildingHelpUrls (t,mb) =
-  case t of
-    City c -> [ img "city", img $ case c of
-                                    Red   -> "city_red"
-                                    Blue  -> "city_blue"
-                                    Green -> "city_green"
-                                    White -> "city_white" ]
-    _      -> case mb of
-                Nothing -> []
-                Just f  -> maybeToList (featureHelpUrl f)
-  where
-  img x = "img" </> "manual" </> "features" </> x <.> "png"
-
-
-enemyPowerHelpUrl :: Enemy -> [FilePath]
-enemyPowerHelpUrl Enemy { .. } =
-  coldFireDefense ++
-    concatMap defenseInfo (Set.toList enemyAbilities) ++ attackInfo
-  where
-  attackInfo = case enemyAttack of
-                 AttacksWith el _ ->
-                  case el of
-                    Physycal      -> []
-                    Fire          -> [ url "fire_attack" ]
-                    Ice           -> [ url "ice_attack" ]
-                    ColdFire      -> [ url "cold_fire_attack" ]
-
-                 Summoner         -> [ url "summon_attack" ]
-
-  coldFireDefense
-    | Resists Fire `Set.member` enemyAbilities &&
-      Resists Ice  `Set.member` enemyAbilities =
-      [ url "fire_and_ice_resistance" ]
-    | otherwise = []
-
-  defenseInfo x = case x of
-                    Fortified     -> [ url "fortified" ]
-                    Resists e ->
-                      case e of
-                        Fire        -> [ url "fire_resistance" ]
-                        Ice         -> [ url "ice_resistance" ]
-                        Physycal    -> [ url "physycal_resistance" ]
-                        ColdFire    -> []
-                    Swift         -> [ url "swift" ]
-                    Brutal        -> [ url "brutal" ]
-                    Poisons       -> [ url "poison" ]
-                    Paralyzes     -> [ url "paralyze" ]
-
-  url x = "img" </> "manual" </> "enemy_abilities" </> x <.> "png"
-
-
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 -- RO
-
-getDeedImage :: Snap ()
-getDeedImage =
-  do deed <- deedParam "deed"
-     serveFile (deedPath deed)
-
-
-getUnitImage :: Snap ()
-getUnitImage =
-  do unit <- unitParam "unit"
-     serveFile (unitPath unit)
-
-
-snapGetMapHelpUrl :: Act
-snapGetMapHelpUrl ref =
-  do a  <- addrParam
-     g  <- liftIO (snapGame `fmap` readIORef ref)
-     sendJSON $
-       object [ "helpUrl" .=
-                  fmap Text.pack
-                     (getBuildingHelpUrls =<<
-                       maybeToList (getFeatureAt a (land g)))
-              , "enemyPowers" .=
-                      nub (concatMap (map Text.pack . enemyPowerHelpUrl)
-                               (getRevealedEnemiesAt a (land g)))
-              , "offerUrls" .=
-                  nub ( fmap (Text.pack . unitUrl) (unitsForHire a g) ++
-                        fmap (Text.pack . deedUrl) (availableDeeds a g) )
-              ]
 
 --------------------------------------------------------------------------------
 
