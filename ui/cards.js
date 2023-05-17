f06fd148e0d0ac0521f82ebcdf59f6621638c936
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-05-16 19:22:47 -0700

    Fix drawing of cards

--- ui/cards.js
+++ ui/cards.js
@@ -1,11 +1,5 @@
 function newCards() {
 
-  function joinName(x,y) { return x + " / " + y }
-
-  function getName(deed) {
-    if (!deed.deedNamePower) return deed.deedName
-    return joinName(deed.deedName,deed.deedNamePower)
-  }
 
   const dims =
     { Action:
@@ -15,33 +9,33 @@ function newCards() {
             [ "March"
             , "Tranquility"
             , "Concentration"
-            , "Savage Harvesting"
+            , "Savage_Harvesting"
             , ""
             , "Rejuvenate"
-            , "Will Focus"
+            , "Will_Focus"
 
             , "Swiftness"
             , "Promise"
-            , "Mana Draw"
-            , "Swift Reflexes"
+            , "Mana_Draw"
+            , "Swift_Reflexes"
             , ""
-            , "Noble Manners"
-            , "Mana Pull"
+            , "Noble_Manners"
+            , "Mana_Pull"
 
             , "Stamina"
             , "Crystallize"
             , "Determination"
             , "Tirelessness"
             , ""
-            , "Crystal Joy"
-            , "Cold Toughness"
+            , "Crystal_Joy"
+            , "Cold_Toughness"
 
             , "Rage"
             , "Threaten"
             , "Improvisation"
-            , "Battle Versatility"
+            , "Battle_Versatility"
             , ""
-            , "Ruthless Coercion"
+            , "Ruthless_Coercion"
             , "Instinct"
             ]
         }
@@ -50,50 +44,50 @@ function newCards() {
         { cols: 8, ho: 0.22, vo: [6.3]
         , boxes: [ [0.57, 0.21], [0.72, 0.37] ]
         , cards:
-            [ "Fire Bolt"
-            , "Ice Bolt"
-            , "Swift Bolt"
-            , "Crushing Bolt"
-            , "Blood Rage"
-            , "Ice Shield"
+            [ "Fire_Bolt"
+            , "Ice_Bolt"
+            , "Swift_Bolt"
+            , "Crushing_Bolt"
+            , "Blood_Rage"
+            , "Ice_Shield"
             , "Agility"
-            , "Refreshing Walk"
+            , "Refreshing_Walk"
 
             , "Intimidate"
-            , "Frost Bridge"
-            , "Song of Wind"
-            , "Path Finding"
-            , "Blood Ritual"
-            , "Pure Magic"
-            , "Heroic Tale"
+            , "Frost_Bridge"
+            , "Song_of_Wind"
+            , "Path_Finding"
+            , "Blood_Ritual"
+            , "Pure_Magic"
+            , "Heroic_Tale"
             , "Regeneration"
 
-            , "Into the Heat"
-            , "Steady Tempo"
+            , "Into_the_Heat"
+            , "Steady_Tempo"
             , "Diplomacy"
-            , "In Need"
+            , "In_Need"
             , "Decompose"
-            , "Crystal Mastery"
-            , "Mana Storm"
+            , "Crystal_Mastery"
+            , "Mana_Storm"
             , "Ambush"
 
-            , "Maximal Effect"
-            , "Magic Talent"
+            , "Maximal_Effect"
+            , "Magic_Talent"
             , "Learning"
             , "Training"
             , "Counterattack"
-            , "Ritual Attack"
-            , "Blood of Ancients"
-            , "Shield Bash"
+            , "Ritual_Attack"
+            , "Blood_of_Ancients"
+            , "Shield_Bash"
 
-            , "Temporal Portal"
-            , "Spell Forge"
+            , "Temporal_Portal"
+            , "Spell_Forge"
             , "Chivalry"
-            , "Peaceful Moment"
-            , "Dodge and Weave"
-            , "Stout Resolve"
-            , "Force of Nature"
-            , "Mountain Lore"
+            , "Peaceful_Moment"
+            , "Dodge_and_Weave"
+            , "Stout_Resolve"
+            , "Force_of_Nature"
+            , "Mountain_Lore"
             ]
          }
 
@@ -108,36 +102,36 @@ function newCards() {
         { cols: 6, ho: 0.24, vo: [0.3,6.5]
         , boxes: [ [0.02, 0.67], [0.5, 0.67] ]
         , cards:
-            [ joinName("Energy Flow"         , "Energy Steal")
-            , joinName("Cure"                , "Disease")
-            , joinName("Meditation"          , "Trance")
-            , joinName("Tremor"              , "Earthquake")
-            , joinName("Underground Travel"  , "Underground Attack")
-            , joinName("Restoration"         , "Rebirth")
-            , joinName("Mana Meltdown"       , "Mana Radiance")
-            , joinName("Demolish"            , "Disintegrate")
-            , joinName("Burning Shield"      , "Exploding Shield")
-            , joinName("Fireball"            , "Firestorm")
-            , joinName("Flame Wall"          , "Flame Wave")
-            , joinName("Offering"            , "Sacrifice")
-            , joinName("Mana Claim"          , "Mana Curse")
-            , joinName("Chill"               , "Lethal Chill")
-            , joinName("Mistw Form"          , "Veil of Mist")
-            , joinName("Snowstorm"           , "Blizzard")
-            , joinName("Space Bending"       , "Time Bending")
-            , joinName("Mana Bolt"           , "Mana Thunderbolt")
-            , joinName("Mind Read"           , "Mind Steal")
-            , joinName("Expose"              , "Mass Expose")
-            , joinName("Call to Arms"        , "Call to Glory")
-            , joinName("Charm"               , "Possess")
-            , joinName("Whirlwind"           , "Tornado")
-            , joinName("Wings of Wind"       , "Wings of Night")
+            [ "Energy_Flow"
+            , "Cure"
+            , "Meditation"
+            , "Tremor"
+            , "Underground_Travel"
+            , "Restoration"
+            , "Mana Meltdown"
+            , "Demolish"
+            , "Burning_Shield"
+            , "Fireball"
+            , "Flame_Wall"
+            , "Offering"
+            , "Mana_Claim"
+            , "Chill"
+            , "Mist_Form"
+            , "Snowstorm"
+            , "Space_Bending"
+            , "Mana_Bolt"
+            , "Mind_Read"
+            , "Expose"
+            , "Call_to_Arms"
+            , "Charm"
+            , "Whirlwind"
+            , "Wings_of_Wind"
             ]
         }
 
     , Wound:
         { cols: 1, ho: 0.22, vo: [11.4]
-        , cards: ["Wound"]
+        , cards: ["WoundName"]
         }
     }
     const cardLoc = {}
@@ -145,16 +139,27 @@ function newCards() {
       const inf = dims[ty]
       const cs = inf.cards
       const cols = inf.cols
+      console.log(cs)
       for (let i = 0; i < cs.length; ++i) {
         const r = Math.floor(i / cols)
         cardLoc[cs[i]] = [ r, i - r * cols ]
       }
     }
 
+  function getDeedType(deed) {
+    for (const ty in dims) {
+      const cards = dims[ty].cards
+      for (let i = 0; i < cards.length; ++i) {
+        if (cards[i] === deed.deedName) return ty
+      }
+    }
+  }
+
   function drawDeed(deed) {
-    const name    = getName(deed)
+    const name    = deed.deedName
+    console.log(name)
     const [r,c]   = cardLoc[name]
-    const cardTy  = deed.deedType.tag
+    const cardTy  = getDeedType(deed)
     const info    = dims[cardTy]
 
     function small() {
@@ -218,7 +223,7 @@ function newCards() {
   }
 
   const obj = {}
-  obj.getName     = getName
+  obj.getName     = (d) => d.deedName
   obj.drawDeed    = drawDeed
 
   return obj
