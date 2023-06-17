6385cd093eec10467ee0bd5af45e819a871d1b80
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2023-06-17 10:56:06 -0700

    Add crystals to the state

--- src/Mana/Pool.hs
+++ src/Mana/Pool.hs
@@ -1,11 +1,18 @@
 module Mana.Pool
   ( ManaPool
   , emptyManaPool
+
+    -- * Normal Mana
   , addSourceMana, addMana
   , hasMana
   , removeMana
   , removeManaMaybe
   , convertMana
+
+    -- * Crystals
+  , addCrystal
+  , useCrystal
+  , hasCrystal
   ) where
 
 import GHC.Generics
@@ -19,14 +26,16 @@ import Mana.Type
 
 
 data ManaPool = ManaPool
-  { mpMana   :: Bag Mana    -- ^ May be discarded
-  , mpSource :: Bag Mana    -- ^ Has toi be used before end of round
+  { mpMana      :: Bag Mana       -- ^ May be discarded
+  , mpSource    :: Bag Mana       -- ^ Has to be used before end of round
+  , mpCrystals  :: Bag BasicMana  -- ^ Persists across rounds
   } deriving (Generic, ToJSON)
 
 emptyManaPool :: ManaPool
 emptyManaPool = ManaPool
-  { mpMana = bagEmpty
-  , mpSource = bagEmpty
+  { mpMana      = bagEmpty
+  , mpSource    = bagEmpty
+  , mpCrystals  = bagEmpty
   }
 
 addSourceMana :: Mana -> ManaPool -> ManaPool
@@ -34,6 +43,18 @@ addSourceMana m mp = mp { mpSource = bagChange 1 m (mpSource mp) }
 
 addMana :: Mana -> ManaPool -> ManaPool
 addMana m mp = mp { mpMana = bagChange 1 m (mpMana mp) }
+
+addCrystal :: BasicMana -> ManaPool -> ManaPool
+addCrystal m mp = mp { mpCrystals = bagChange 1 m (mpCrystals mp) }
+
+useCrystal :: BasicMana -> ManaPool -> ManaPool
+useCrystal m mp =
+  case bagChangeMaybe (-1) m (mpCrystals mp) of
+    Just newCrystals -> addMana (BasicMana m) mp { mpCrystals = newCrystals }
+    Nothing -> mp
+
+hasCrystal :: BasicMana -> ManaPool -> Bool
+hasCrystal m mp = bagContains m (mpCrystals mp) > 0
 
 convertMana :: Mana -> Mana -> ManaPool -> ManaPool
 convertMana fromM toM mp =
